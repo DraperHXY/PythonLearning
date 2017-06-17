f4e9e975ca197d9f510a546cc54c535711873db7
DraperHXY <draper_hxy@163.com>
2017-06-18 00:00:26 +0800

    循环 range

---
+++ com/draper/baseobject/range/range.py
@@ -0,0 +1,6 @@
+# print(range(5))
+# print(list({0, 1, 2, 3, 4}))
+# print(list(tuple({0, 1, 2, 3, 4})))
+# print({0, 1, 2, 3, 4, })
+# print(list(range(5)), sep=str(" haha "))
+
---
+++ com/draper/baseobject/set/set.py
@@ -0,0 +1,8 @@
+s = set(['Adam', 'Lisa', 'Paul'])
+L = ['Adam', 'Lisa', 'Bart', 'Paul']
+print(L[0], L[1], L[2], L[3], sep=" haha ")
+for x in L:
+    if x in s:
+        s.remove(x)
+    else:
+        s.add(x)
